c346584d2675533efd0f2e3fe75b38e80410c95b
Shayunak <shayanhamidi2000@gmail.com>
2020-05-16 22:36:28 +0430

    base modules for DP

---
+++ DP.v
@@ -0,0 +1,5 @@
+module DP(input[2:0] AluOperation,input[1:0] PCSrc,input[1:0] AluSrcB,input rst,clk,PcSrc,link,RegDst,RegWrite,AluSrcA,
+	IRWrite,MemWrite,MemRead,MemtoReg,PCWrite,PCWriteCond,branch,output[5:0] func,output[5:0] opcode);
+		
+	
+endmodule
